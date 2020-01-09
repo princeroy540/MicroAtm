78098d4c87d2403e9660e408a9efede2c68a78b5
prince <princeroy540@gmail.comm>
2020-01-10 03:17:00 +0530

    init

---
+++ src/main/java/com/MicroAtm/MicroAtm/MicroAtmApplication.java
@@ -0,0 +1,13 @@
+package com.MicroAtm.MicroAtm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAtmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroAtmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MicroAtm/MicroAtm/MicroAtmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroAtm.MicroAtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroAtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
